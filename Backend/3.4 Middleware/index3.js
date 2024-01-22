a9dcc9f4bc1a90cd2c0808ad606b01cbfbbc6070
MahatiMadhira <93003959+MahatiMadhira@users.noreply.github.com>
2024-01-21 20:52:24 -0500

    middleware

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -1,8 +1,13 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
+function logger(req, res, next) {
+  console.log("Request method: ",req.method);
+  console.log("Request URL: ",req.url);
+  next();
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
