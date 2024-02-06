4508967e3ab613a82c774ce149ef5f6ac5866cc1
MahatiMadhira <93003959+MahatiMadhira@users.noreply.github.com>
2024-02-05 20:30:20 -0500

    EJS partials

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// import generateName from "sillyname";
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+import superheroes from "superheroes";
+
+var superheroname = superheroes.random();
+
+console.log(`My superhero name is ${superheroname}!`);
---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,90 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).on("keypress", function() {
+    if(!started) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+    
+});
+
+$(".btn").on("click", function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+        //console.log("wrong");
+
+    }
+
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    console.log(gamePattern);
+
+    $("#" + randomChosenColour).fadeTo(100, 0.3, function() { 
+        $(this).fadeTo(500, 1.0); 
+    });
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
